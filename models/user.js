35861b3f9c5925d05e030067d315f1f331525c00
S00ahKim <rlatndk0206@naver.com>
2018-11-23 05:30:55 +0900

    kakao login

--- models/user.js
+++ models/user.js
@@ -5,13 +5,15 @@ const bcrypt = require('bcrypt');
 const Schema = mongoose.Schema;
 
 /* DB schema
-  (이름, 이메일(PK), 비밀번호, 페이스북 정보, 등급(회원/매니저)++*/
+  (이름, 이메일(PK), 비밀번호, 페이스북 정보, 카카오톡 정보, 등급(회원/매니저)++, 즐겨찾기*/
 var schema = new Schema({
   name: {type: String, required: true, trim: true},
   email: {type: String, required: true, index: true, unique: true, trim: true},
   password: {type: String},
   facebook: {id: String, token: String, photo: String},
+  kakaotalk: {id: String, token: String, photo: String},
   role: {type:String},
+  favorite: [{type: Schema.Types.ObjectId, ref: 'Comp_info'}],
   createdAt: {type: Date, default: Date.now}
 }, {
   toJSON: { virtuals: true},
--- routes/auth.js
+++ routes/auth.js
@@ -4,19 +4,35 @@ module.exports = (app, passport) => {
   });
 
   app.post('/signin', passport.authenticate('local-signin', {
-    successRedirect : '/comp_infos', // redirect to the secure profile section
-    failureRedirect : '/signin', // redirect back to the signup page if there is an error
-    failureFlash : true // allow flash messages
+    successRedirect: '/comp_infos', // redirect to the secure profile section
+    failureRedirect: '/signin', // redirect back to the signup page if there is an error
+    failureFlash: true // allow flash messages
   }));
 
+  //- 카카오 로그인
+  app.get('/auth/kakao',
+    passport.authenticate('kakao-login')
+  );
+
+  app.get('/auth/kakao/callback',
+    passport.authenticate('kakao-login', {
+      failureRedirect: '/',
+      failureFlash: true
+    }), (req, res, next) => {
+      req.flash('success', '반갑습니다!');
+      res.redirect('/comp_infos');
+    }
+  );
+
+  //- 페이스북 로그인
   app.get('/auth/facebook',
-    passport.authenticate('facebook', { scope : 'email' })
+    passport.authenticate('facebook', { scope: 'email' })
   ); //-누구인지 찾음.
 
   app.get('/auth/facebook/callback',
     passport.authenticate('facebook', {
-      failureRedirect : '/signin',
-      failureFlash : true // allow flash messages
+      failureRedirect: '/signin',
+      failureFlash: true // allow flash messages
     }), (req, res, next) => {
       req.flash('success', '반갑습니다!');
       res.redirect('/comp_infos');
