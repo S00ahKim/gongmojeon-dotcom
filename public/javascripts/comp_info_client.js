78d15d32b85fd50857a573a76348354e23a67f43
S00ahKim <rlatndk0206@naver.com>
2018-11-22 21:32:16 +0900

    javascript ++

---
+++ public/javascripts/comp_info_client.js
@@ -0,0 +1,47 @@
+$(function() {
+  $('.comp_info-like-btn').click(function(e) {
+    var $el = $(e.currentTarget);
+    if ($el.hasClass('loading')) return;
+    $el.addClass('loading');
+    $.ajax({
+      url: '/api/comp_infos/' + $el.data('id') + '/like',
+      method: 'POST',
+      dataType: 'json',
+      success: function(data) {
+        $('.comp_info .num-likes').text(data.numLikes);
+        $('.comp_info-like-btn').hide();
+      },
+      error: function(data, status) {
+        if (data.status == 401) {
+          alert('로그인이 필요합니다.');
+          location = '/signin';
+        }
+        console.log(data, status);
+      },
+      complete: function(data) {
+        $el.removeClass('loading');
+      }
+    });
+  });
+
+  $('.comment-like-btn').click(function(e) {
+    var $el = $(e.currentTarget);
+    if ($el.hasClass('disabled')) return;
+    $.ajax({
+      url: '/api/comments/' + $el.data('id') + '/like',
+      method: 'POST',
+      dataType: 'json',
+      success: function(data) {
+        $el.parents('.comment').find('.num-likes').text(data.numLikes);
+        $el.addClass('disabled');
+      },
+      error: function(data, status) {
+        if (data.status == 401) {
+          alert('로그인이 필요합니다.');
+          location = '/signin';
+        }
+        console.log(data, status);
+      }
+    });
+  });
+}); 
