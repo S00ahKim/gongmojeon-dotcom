9229d30703591925676c17b594e1cc0c7f272b8b
S00ahKim <rlatndk0206@naver.com>
2018-12-10 21:54:14 +0900

    notice

--- routes/index.js
+++ routes/index.js
@@ -6,4 +6,16 @@ router.get('/', function(req, res, next) {
   res.render('index');
 });
 
+router.get('/notice', function(req, res, next){
+  res.render('notice');
+})
+
+router.get('/img', function(req, res, next){
+  res.render('img');
+})
+
+router.get('/developer', function(req, res, next){
+  res.render('developer');
+})
+
 module.exports = router;
